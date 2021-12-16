56df4c10502553ff5db389a282f47de0d09678be
StickyPine <pine@pop-os.localdomain>
2021-12-16 15:59:37 +0100

    clean

---
+++ python/fast_exp_mod.py
@@ -0,0 +1,19 @@
+def fast_pow_mod(b, e, m):
+    mask = 1
+    res = 1
+    l_val = b
+    while mask <= e:
+        if (mask & e) == mask:
+            res = (res * l_val) % m
+        mask = mask << 1
+        l_val = (l_val * l_val) % m
+    return res
+
+
+b = 42
+e = 1000000000000000000000
+m = 423141234
+x = fast_mod_pow2(b, 0, m)
+# y = pow(b, e, m)
+print(x)
+# print(y)
