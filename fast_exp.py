00795c1eef80e7dd33ee84ad4e8dbc498d979677
StickyPine <pine@pop-os.localdomain>
2021-12-16 13:09:17 +0100

    rename

--- fast_exp.py
+++ fast_exp.py
@@ -1,4 +1,4 @@
-def getHeavyBit(x):
+def getBiggest2Pow(x):
     res = 1
     while True:
         n = res << 1
@@ -7,7 +7,7 @@ def getHeavyBit(x):
         res = n
 
 def fast_mod_pow(b, e, n):
-    mask = getHeavyBit(e)
+    mask = getBiggest2Pow(e)
     res = b
     while mask > 1:
         mask = mask >> 1
