fcc2ffee71876381b6a06808bcf571f725facc6f
StickyPine <pine@pop-os.localdomain>
2021-12-16 16:34:05 +0100

    rename

--- python/fast_pow_mod.py
+++ python/fast_pow_mod.py
@@ -1,11 +1,11 @@
 def fast_pow_mod(b, e, m):
     mask = 1
     res = 1
-    l_val = b
+    pow2 = b
     while mask <= e:
         if (mask & e) == mask:
-            res = (res * l_val) % m
-        l_val = (l_val * l_val) % m
+            res = (res * pow2) % m
+        pow2 = (pow2 * pow2) % m
         mask <<= 1
     return res
 
@@ -13,7 +13,7 @@ def fast_pow_mod(b, e, m):
 b = 42
 e = 1000000000000000000000
 m = 423141234
-x = fast_pow_mod(b, e, m)
+x = fast_pow_mod(3, 4, m)
 # y = pow(b, e, m)
 print(x)
 # print(y)
