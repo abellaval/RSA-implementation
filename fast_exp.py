c36ddd3a036819ff6a57d80f7651c8d5cad8c37a
StickyPine <pine@pop-os.localdomain>
2021-12-16 15:16:40 +0100

    ajout d'une autre version

--- fast_exp.py
+++ fast_exp.py
@@ -6,20 +6,39 @@ def getBiggest2Pow(x):
             return res
         res = n
 
-def fast_mod_pow(b, e, n):
+def fast_mod_pow(b, e, m):
     mask = getBiggest2Pow(e)
     res = b
     while mask > 1:
         mask = mask >> 1
-        res = (res * res) % n
+        res = (res * res) % m
         if (mask & e) == mask:
-            res = (res * b) % n
+            res = (res * b) % m
+    return res
+
+def fast_mod_pow2(b, e, m):
+    mask = 1
+    mask_index = 0
+    l_index = 0
+    l_val = b
+    res = 1
+    while mask <= e:
+        if (mask & e) == mask:
+            while l_index < mask_index:
+                l_val = (l_val * l_val) % m
+                l_index += 1
+            res = (res * l_val) % m
+        mask_index += 1
+        mask = mask << 1
     return res
 
 
 b = 42
 e = 1000000000000000000000
 m = 423141234
-x = fast_mod_pow(b, e, m)
-# x = pow(b, e, m)
-print(x)
+# x = fast_mod_pow(b, e, m)
+# y = fast_mod_pow2(b, e, m)
+z = pow(b, e, m)
+# print(x)
+# print(y)
+print(z)
