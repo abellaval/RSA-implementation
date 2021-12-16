7548cb4782612b785e63d5e89c6b876c167f84f5
StickyPine <pine@pop-os.localdomain>
2021-12-16 16:54:37 +0100

    evite une multiplication inutile

--- python/fast_pow_mod.py
+++ python/fast_pow_mod.py
@@ -2,18 +2,19 @@ def fast_pow_mod(b, e, m):
     mask = 1
     res = 1
     pow2 = b
-    while mask <= e:
+    while True: # n + 1/2 loop
         if (mask & e) == mask:
             res = (res * pow2) % m
+        if (mask >=e):
+            return res
         pow2 = (pow2 * pow2) % m
         mask <<= 1
-    return res
 
 
 b = 42
 e = 1000000000000000000000
 m = 423141234
-x = fast_pow_mod(3, 4, m)
+x = fast_pow_mod(3, 3, m)
 # y = pow(b, e, m)
 print(x)
 # print(y)
