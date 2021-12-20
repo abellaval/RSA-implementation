4ba9c617ac590cf1d9900cdaa64c38a3c6f4f8a1
StickyPine <pine@pop-os.localdomain>
2021-12-20 13:16:07 +0100

    une variable en moins

--- Romain/fast_pow_mod.py
+++ Romain/fast_pow_mod.py
@@ -1,20 +1,19 @@
 def fast_pow_mod(b, e, m):
-    mask = 1
     res = 1
     b_pow2 = b
     while True: # n + 1/2 loop
-        if (mask & e) == mask:
+        if e & 1:
             res = (res * b_pow2) % m
-        if mask >= e:
+        if e==0:
             return res
         b_pow2 = (b_pow2 * b_pow2) % m
-        mask <<= 1
+        e >>= 1
 
 
 b = 42
 e = 1000000000000000000000
 m = 423141234
-x = fast_pow_mod(3, 3, m)
+x = fast_pow_mod(2, 6, m)
 # y = pow(b, e, m)
 print(x)
 # print(y)
