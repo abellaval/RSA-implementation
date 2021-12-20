d12153483b7ee78f899505d08160ea2e273a675f
tlambilo <tanguy.lambilot@ulb.be>
2021-12-20 08:13:59 +0100

    ajout corps defonctions

--- Tanguy/template RSA.py
+++ Tanguy/template RSA.py
@@ -1,3 +1,6 @@
+import math
+import secrets
+
 '''
 Fonctions pour une implémentation basique de RSA.
 Devrait permettre de générer des premiers p et q de taille 2^20 (~un million)
@@ -15,13 +18,16 @@ de manière un peu sérieuse.
 '''
 
 
-
 # Fichier fonctions
-def pgcd(a,b) : 
+def pgcd(a, b) : 
+    return math.gcd(a, b)
 
-def euclide(a,n) :
 
-def inverse_mod(x,p)
+def euclide(a, n) :
+    return
+
+
+def inverse_mod(x,p) :
     """
     retourne l'inverse de x modulo p,
     c'est-à-dire l'entier y dans [1,p-1] tel que
@@ -31,14 +37,31 @@ def inverse_mod(x,p)
     on ne force pas y à être positif, il faut donc vraiment retourner
     y dans [1,p-1].
     """
+    # méthode naïve
+    if pgcd(x, p) != 1 :
+        return False
+    
+    for y in range(1, p):
+        if y*x % p == 1 :
+            return y
+    # Peut être amélioré grâce aux coeff de Bézout
 
 
 def is_prime(n) :
     """
     Un test de primalité naïf (voir slides de présentation sur Teams)
-    Vous pourrez cette fonction par une implémentation de l'algo de
+    Vous pourrez améliorer cette fonction par une implémentation de l'algo de
     Miller-Rabin, à terme
     """
+    borne_inf = 2
+    borne_sup = math.floor(math.sqrt(n))
+
+    c = borne_inf
+
+    while c < borne_sup and pgcd(n, c) == 1:
+        c += 1 
+
+    return c == borne_sup
 
 
 def exp(a,x,p):
@@ -46,17 +69,34 @@ def exp(a,x,p):
     Calcule a^x mod p
     avec la méthode de l'exponentiation rapide
     """
-
+    res = 1
+    v = a
+    loop = int(math.log2(x)+1)
+    for _ in range(loop):
+        if x % 2 == 1:
+            res = (res * v) % p
+        x >>= 1
+        v = (v ** 2) % p 
+    return res
     
 
-
-
 # Fichier main
+def genPrimeNb(k):
+
+    prime = False
+    num = 0
+
+    while not prime:
+        num = secrets.randbits(k)
+        prime = is_prime(num)
+
+    return num
+
 def KeyGen(k) : 
     """
     Génère des celfs RSA à partir de deux nombres premiers p et q,
     chacun étant des entiers de k-bits.
-    Le module N est donc un entier d'neviron 2k-bits.
+    Le module N est donc un entier d'environ 2k-bits.
     Par souci de simplicité, on peut poser directement e = 65537,
     à condition évidemment que phi = (p-1)(q-1) soit plus petit que e.
 
@@ -65,6 +105,16 @@ def KeyGen(k) :
     Input : Le paramètre de sécurité k
     Output : Le tuple ((N,e),(N,d)) représentant les clefs
     """
+    p = genPrimeNb(k//2)
+    q = genPrimeNb(k//2)
+
+    N = p*q
+    phi_N = (p-1)*(q-1)
+
+    e = 65537
+    d = inverse_mod(e, phi_N)
+
+    return ((N, e), (N, d))
 
 
 def E(m,e,N) :
@@ -78,8 +128,10 @@ def E(m,e,N) :
     Input : Un message m ; l'exposant e ; le module N
     Output : Un chiffré c
     """
+    return exp(m, e, N)
 
-def D(c,d,N) :
+
+def D(c,d, N) :
     """
     Déchiffre un chiffré c avec la clef privée SK = (N,d).
     c et le message rendu en sortie doivent être des entiers,
@@ -90,3 +142,4 @@ def D(c,d,N) :
     Input : Un chiffré c ; l'exposant d ; le module N
     Output : Un message m
     """  
+    return exp(c, d, N)
