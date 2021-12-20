700b4bbeacd4450c63d928dcd42c517afa7d7946
StickyPine <pine@pop-os.localdomain>
2021-12-20 13:28:41 +0100

    cette fois ci vrmt j'y touche plus

--- Romain/fast_pow_mod.py
+++ Romain/fast_pow_mod.py
@@ -1,18 +1,17 @@
 def fast_pow_mod(b, e, m):
     res = 1
-    while True: # n + 1/2 loop
+    while e > 0 :
         if e & 1:
             res = (res * b) % m
-        if e==0:
-            return res
         b = (b * b) % m
         e >>= 1
+    return res
 
 
 b = 42
 e = 1000000000000000000000
 m = 423141234
-x = fast_pow_mod(2, 6, m)
+x = fast_pow_mod(2, 7, m)
 # y = pow(b, e, m)
 print(x)
 # print(y)
