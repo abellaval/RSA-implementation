08c7b006b208af0758ab8248c5513bcc7a66ce15
vpiryns <Victor.Piryns@ulb.be>
2022-02-11 20:53:11 +0100

    Adds basic fingerprinting.

--- website/client/__init__.py
+++ website/client/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response
+from hashlib import sha256
 
 app = Flask(__name__)
 
@@ -6,3 +7,21 @@ app = Flask(__name__)
 @app.route("/")
 def homepage():
     return render_template("home.html")
+
+
+@app.route("/fingerprint/", methods=["POST"])
+def fingerprint():
+    # TODO: Add cookie to server side
+    # TODO: Add expiration date (12~24h?)
+    response = Response(
+        headers=[
+            ("Set-Cookie",
+             "fingerprint={visitor_id}@{hashed_ip}; HttpOnly; Secure; SameSite=Strict".format(
+                 visitor_id=request.get_json(),
+                 hashed_ip=sha256(
+                     request.remote_addr.encode("ascii")
+                 ).hexdigest())),
+            ("Content-Type", "application/json")
+        ]
+    )
+    return response
---
+++ website/client/static/js/fingerprint.js
@@ -0,0 +1,22 @@
+const fpPromise = import('https://openfpcdn.io/fingerprintjs/v3')
+    .then(FingerprintJS => FingerprintJS.load())
+
+fpPromise
+    .then(fp => fp.get())
+    .catch(error => console.error(`Failed to get fingerprint \nError:${error}`))
+    .then(result => {
+        generateFingerprint(window.location.href + "/fingerprint", result.visitorId)
+    })
+    .catch(error => console.error(`Failed to store fingerprint \nError:${error}`))
+
+function generateFingerprint(url, visitorId) {
+    fetch(url, {
+        method: 'POST',
+        mode: 'same-origin',
+        cache: 'no-store',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(visitorId)
+    }).catch(error => console.error("Failed to setup fingerprint!"))
+}
