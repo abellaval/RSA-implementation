e0830749be1e70da9bac39a63bc4afb22fabc343
vpiryns <Victor.Piryns@ulb.be>
2022-02-16 13:14:25 +0100

    Update to Election model in admin server

--- website/admin/models/election.py
+++ website/admin/models/election.py
@@ -3,6 +3,8 @@ from website.admin import db
 
 class Election(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    description = db.Column(db.String(240), nullable=True)
     candidates = db.relationship("Candidate", backref="election")
     voters_participating = db.relationship("VoterParticipation",
                                            backref="election")
