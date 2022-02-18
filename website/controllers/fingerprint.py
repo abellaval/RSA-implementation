0845a7252b128af6bd1cc4ec852a0ea6e198da43
vpiryns <Victor.Piryns@ulb.be>
2022-02-18 14:39:53 +0100

    Remove port from fingerprint, as it changes for each request, even from same user

--- website/controllers/fingerprint.py
+++ website/controllers/fingerprint.py
@@ -5,6 +5,4 @@ from flask import Request
 def generate_fingerprint(request: Request):
     visitor_id = request.get_json()
     hashed_ip = sha256(request.remote_addr.encode("ascii")).hexdigest()
-    hashed_port = sha256(str(request.environ.get(
-        "REMOTE_PORT")).encode("ascii")).hexdigest()
-    return f"{visitor_id}@{hashed_ip}:{hashed_port}"
+    return f"{visitor_id}@{hashed_ip}"
