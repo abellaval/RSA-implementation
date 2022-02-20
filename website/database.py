fd13ed873f4450c4882b12be0aa0fd08f727fcbc
vpiryns <Victor.Piryns@ulb.be>
2022-02-20 15:58:54 +0100

    Update problem with path of DB based on who is __main__

--- website/database.py
+++ website/database.py
@@ -4,10 +4,10 @@ from flask import g
 DATABASE_PATH = "website/database.sqlite"
 
 
-def get_db():
+def get_db(path=DATABASE_PATH):
     db = getattr(g, "database", None)
     if db is None:
-        db = g.database = sqlite3.connect(DATABASE_PATH)
+        db = g.database = sqlite3.connect(path)
     db.row_factory = sqlite3.Row
     return db
 
@@ -22,7 +22,7 @@ def clone_connection(exception):
 if __name__ == "__main__":
     from website.app import app
     with app.app_context():
-        db = get_db()
+        db = get_db("database.sqlite")
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
