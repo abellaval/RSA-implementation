6e1fbe3ea26aaafe76d814594014f1df6f313d02
vpiryns <Victor.Piryns@ulb.be>
2022-03-06 13:09:08 +0100

    Remove browser fp from fingerprint (not reliable)

--- website/fingerprint.py
+++ website/fingerprint.py
@@ -5,4 +5,5 @@ from flask import Request
 def generate_fingerprint(request: Request):
     visitor_id = request.get_json()
     hashed_ip = sha256(request.remote_addr.encode("ascii")).hexdigest()
-    return f"{visitor_id}@{hashed_ip}"
+    # return f"{visitor_id}@{hashed_ip}"
+    return str(hashed_ip)
